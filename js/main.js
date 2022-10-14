a1e36d13b76a9535720aaab90408cde74902ad1f
owentashlin <owentashlin@gmail.com>
2022-10-13 18:37:54 -0700

    added hover color changes

--- js/main.js
+++ js/main.js
@@ -7,24 +7,46 @@ const bluButton = document.querySelector('#blue-quad')
 const yelButton = document.querySelector('#yellow-quad')
 
 //functions
+
+//hover functions
+hoverRedBtn = function() {
+    console.log('green button hover')
+}
+hoverGrnBtn = function() {
+    console.log('red button hover')
+}
+hoverBluBtn = function() {
+    console.log('blue button hover')
+}
+hoverYelBtn = function() {
+    console.log('yellow button hover')
+}
+
+//click functions
 clickGrnBtn = function() {
     console.log('green button clicked')
 }
-
 clickRedBtn = function() {
     console.log('red button clicked')
 }
-
 clickBluBtn = function() {
     console.log('blue button clicked')
 }
-
 clickYelBtn = function() {
     console.log('yellow button clicked')
 }
 
 //event listeners
+
+//hover listeners
+grnButton.addEventListener('mouseover', hoverGrnBtn)
+redButton.addEventListener('mouseover', hoverRedBtn)
+bluButton.addEventListener('mouseover', hoverBluBtn)
+yelButton.addEventListener('mouseover', hoverYelBtn)
+
+//click listeners
 grnButton.addEventListener('click', clickGrnBtn)
 redButton.addEventListener('click', clickRedBtn)
 bluButton.addEventListener('click', clickBluBtn)
 yelButton.addEventListener('click', clickYelBtn)
+
