8fd6d9eb93b99a178961b44cfa8436c74e9384d3
owentashlin <owentashlin@gmail.com>
2022-10-13 21:19:46 -0700

    added styles/function - light up buttons

--- js/main.js
+++ js/main.js
@@ -8,41 +8,37 @@ const yelButton = document.querySelector('#yellow-quad')
 
 //functions
 
-//hover functions
-hoverRedBtn = function() {
-    console.log('green button hover')
-}
-hoverGrnBtn = function() {
-    console.log('red button hover')
-}
-hoverBluBtn = function() {
-    console.log('blue button hover')
-}
-hoverYelBtn = function() {
-    console.log('yellow button hover')
-}
-
 //click functions
 clickGrnBtn = function() {
+    grnButton.style.backgroundColor = '#03cb03';
+    setTimeout(() => {
+        grnButton.style.backgroundColor = 'rgb(0, 61, 0)'
+      }, 500);
     console.log('green button clicked')
 }
 clickRedBtn = function() {
+    redButton.style.backgroundColor = '#ff0000'
+    setTimeout(() => {
+        redButton.style.backgroundColor = 'rgb(64, 1, 1)'
+      }, 500);
     console.log('red button clicked')
 }
 clickBluBtn = function() {
+    bluButton.style.backgroundColor = '#0000ff'
+    setTimeout(() => {
+        bluButton.style.backgroundColor = 'rgb(2, 2, 83)'
+      }, 500);
     console.log('blue button clicked')
 }
 clickYelBtn = function() {
+    yelButton.style.backgroundColor = '#ffff00'
+    setTimeout(() => {
+        yelButton.style.backgroundColor = 'rgb(43, 43, 1)'
+      }, 500);
     console.log('yellow button clicked')
 }
 
 //event listeners
-
-//hover listeners
-grnButton.addEventListener('mouseover', hoverGrnBtn)
-redButton.addEventListener('mouseover', hoverRedBtn)
-bluButton.addEventListener('mouseover', hoverBluBtn)
-yelButton.addEventListener('mouseover', hoverYelBtn)
 
 //click listeners
 grnButton.addEventListener('click', clickGrnBtn)
