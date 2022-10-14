8e834dfeb4afa9ab6515ccfdf56c54875cfc9287
owentashlin <owentashlin@gmail.com>
2022-10-13 18:11:40 -0700

    added basic layout and began coding functionality
    added initial page layout, created buttons and added event listeners. Confirmed event listeners are working.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 console.log('hello')
+
+//elements
+const grnButton = document.querySelector('#green-quad')
+const redButton = document.querySelector('#red-quad')
+const bluButton = document.querySelector('#blue-quad')
+const yelButton = document.querySelector('#yellow-quad')
+
+//functions
+clickGrnBtn = function() {
+    console.log('green button clicked')
+}
+
+clickRedBtn = function() {
+    console.log('red button clicked')
+}
+
+clickBluBtn = function() {
+    console.log('blue button clicked')
+}
+
+clickYelBtn = function() {
+    console.log('yellow button clicked')
+}
+
+//event listeners
+grnButton.addEventListener('click', clickGrnBtn)
+redButton.addEventListener('click', clickRedBtn)
+bluButton.addEventListener('click', clickBluBtn)
+yelButton.addEventListener('click', clickYelBtn)
