86b12e53614cbf953ce39ec9e8728e3aeda3cb5a
owentashlin <owentashlin@gmail.com>
2022-10-16 13:14:15 -0700

    started working on player turn function

--- js/main.js
+++ js/main.js
@@ -17,6 +17,43 @@ let level = 0
 
 //**functions**//
 
+//color button functions - until we figure out how to do it better, with fucking toggle
+
+clickGrnBtn = function() {
+    grnButton.style.backgroundColor = '#03cb03';
+    setTimeout(() => {
+        grnButton.style.backgroundColor = 'rgb(0, 61, 0)'
+      }, 500);
+      playerSequence.push('green')
+    console.log(playerSequence, 'green button clicked')
+}
+
+clickRedBtn = function() {
+    redButton.style.backgroundColor = '#ff0000'
+    setTimeout(() => {
+        redButton.style.backgroundColor = 'rgb(64, 1, 1)'
+      }, 500);
+      playerSequence.push('red')
+    console.log(playerSequence, 'red button clicked')
+}
+
+clickBluBtn = function() {
+    bluButton.style.backgroundColor = '#0000ff'
+    setTimeout(() => {
+        bluButton.style.backgroundColor = 'rgb(2, 2, 83)'
+      }, 500);
+      playerSequence.push('blue')
+    console.log(playerSequence, 'blue button clicked')
+}
+
+clickYelBtn = function() {
+    yelButton.style.backgroundColor = '#ffff00'
+    setTimeout(() => {
+        yelButton.style.backgroundColor = 'rgb(43, 43, 1)'
+      }, 500);
+      playerSequence.push('yellow')
+    console.log(playerSequence, 'yellow button clicked')
+}
 //gameplay functions
 nextLevel = function() {
     let colors = ['green', 'red', 'blue', 'yellow']
@@ -25,7 +62,6 @@ nextLevel = function() {
     //push color into game array
     gameSequence.push(randomClr)
     //when level goes up, number of randomBtn's returned goes up to match level#
-    level += 1
     console.log(gameSequence, level)
 }
 
@@ -36,12 +72,19 @@ clickStartBtn = function () {
 }
 
 playerTurn = function() {
+    if (playerSequence === gameSequence) { 
+        level += 1
+        nextLevel()
+    } else {
+        clickResetBtn
+        console.log('oh no!!!')
+    }
     //player clicks buttons, 
     //loads into playerSequence[]
-    playerSequence.push(playerTurn)
     //forEach playerSequence[]
     //if match gameSequence[]
     //nextLevel is called
+    console.log(playerSequence)
 }
 
 clickResetBtn = function() {
@@ -52,39 +95,6 @@ clickResetBtn = function() {
     console.log(gameSequence, playerSequence, level, 'reset button clicked')
 }
 
-//color button functions - until we figure out how to do it better, with fucking toggle
-
-clickGrnBtn = function() {
-    grnButton.style.backgroundColor = '#03cb03';
-    setTimeout(() => {
-        grnButton.style.backgroundColor = 'rgb(0, 61, 0)'
-      }, 500);
-    console.log('green button clicked')
-}
-
-clickRedBtn = function() {
-    redButton.style.backgroundColor = '#ff0000'
-    setTimeout(() => {
-        redButton.style.backgroundColor = 'rgb(64, 1, 1)'
-      }, 500);
-    console.log('red button clicked')
-}
-
-clickBluBtn = function() {
-    bluButton.style.backgroundColor = '#0000ff'
-    setTimeout(() => {
-        bluButton.style.backgroundColor = 'rgb(2, 2, 83)'
-      }, 500);
-    console.log('blue button clicked')
-}
-
-clickYelBtn = function() {
-    yelButton.style.backgroundColor = '#ffff00'
-    setTimeout(() => {
-        yelButton.style.backgroundColor = 'rgb(43, 43, 1)'
-      }, 500);
-    console.log('yellow button clicked')
-}
 
 //**event listeners**//
 
