d2a27b5a184d381dac085939bac50b3499258fd1
owentashlin <owentashlin@gmail.com>
2022-10-17 11:30:04 -0700

    starting to work on gameSequence function

--- js/main.js
+++ js/main.js
@@ -5,15 +5,10 @@ console.log("sigh... Let's try this again.")
 //buttons
 const grnButton = document.querySelector('#green-btn')
 const redButton = document.querySelector('#red-btn')
-const bluButton = document.querySelector('#blue-btn')
+const bluButton = document.querySelector('#blu-btn')
 const yelButton = document.querySelector('#yellow-btn')
 const startButton = document.querySelector('#start-button')
 const resetButton = document.querySelector('#reset-button')
-
-//gameplay elements
-let gameSequence = []
-let playerSequence = []
-let level = 0
 
 //**functions**//
 
@@ -41,8 +36,8 @@ clickBluBtn = function() {
     bluButton.style.backgroundColor = '#0000ff'
     setTimeout(() => {
         bluButton.style.backgroundColor = 'rgb(2, 2, 83)'
-      }, 500);
-      playerSequence.push('blue')
+    }, 500);
+        playerSequence.push('blue')
     console.log(playerSequence, 'blue button clicked')
 }
 
@@ -55,41 +50,48 @@ clickYelBtn = function() {
     console.log(playerSequence, 'yellow button clicked')
 }
 
-//gameplay functions
-
-nextLevel = function() {
-    let colors = ['green', 'red', 'blue', 'yellow']
-    //get random color for gameplay
-    let randomClr = colors[Math.floor(Math.random() * colors.length)]
-    //random color lights up buttons on gameboard
-    //push color into game array
-    gameSequence.push(randomClr)
-    //when level goes up, number of randomBtn's returned goes up to match level#
-    //level # is displayed in the level field on screen
-    let levelText = document.getElementById('#level-counter')
-    // levelText.textContent('level counter:' + level)
-    console.log(gameSequence, level)
-}
+//gameplay functions- currently a HOT mess...
+const colors = ['green', 'red', 'blue', 'yellow']
+let gameSequence = []
+let playerSequence = []
+let level = 1
 
 clickStartBtn = function() {
-    nextLevel()
+    goToNextLevel = true
     startButton.style.color = 'red'
     console.log('start button clicked')
 }
 
+nextLevel = function() {
+    let randomClr = colors[Math.floor(Math.random() * colors.length)]
+    gameSequence.push(randomClr)
+//     get random color for gameplay
+//     random color lights up buttons on gameboard
+//     push color into game array
+    gameSequence.push(randomClr)
+//     when level goes up, number of randomBtn's returned goes up to match level#
+//    level # is displayed in the level field on screen
+//    let levelText = document.getElementById('#level-counter')
+//    levelText.innerText('level counter:' + level)
+    console.log(gameSequence, level)
+}
+
 playerTurn = function() {
-    if (playerSequence === gameSequence) { 
-        level += 1
-        nextLevel()
-    } else {
-        clickResetBtn
-        console.log('oh no!!!')
+    //  player clicks buttons
+    //  button lights up (and plays sound?)
+    //  button color pushed into playerSequence
+    //  playerSequence is compared to gameSequence
+    for (i = 0; i < playerSequence.length; i++) {
+        if (gameSequence[i] !== playerSequence[i]) {
+            goToNextLevel = false
+        }
     }
-    //player clicks buttons, 
-    //loads into playerSequence[]
-    //forEach playerSequence[]
-    //if match gameSequence[]
-    //nextLevel is called
+    //  if no exact match loser message is displayed   
+    if (!goToNextLevel)
+    level += 1 
+    //  if match exact match
+    //     nextLevel is called
+    nextLevel()
     console.log(playerSequence)
 }
 
@@ -100,7 +102,6 @@ clickResetBtn = function() {
     startButton.style.color = 'rgb(0, 133, 133)'
     console.log(gameSequence, playerSequence, level, 'reset button clicked')
 }
-
 
 //**event listeners**//
 
