92ac27579e0ccdb4901f112257092bf8fab1b614
owentashlin <owentashlin@gmail.com>
2022-10-18 10:24:50 -0700

    commit before bug fix attempt
    new gameSequence causing a major slowdown, need to figure out why. Saving in case I have to forcequit if it gets stuck in an endless loop.

--- js/main.js
+++ js/main.js
@@ -60,92 +60,12 @@ function updateLevel() {
   }
 
 clickStartBtn = function() {
-    let firstClr = colors[Math.floor(Math.random() * colors.length)]
-    gameSequence.push(firstClr)
+    // let firstClr = colors[Math.floor(Math.random() * colors.length)]
+    // gameSequence.push(firstClr)
     startButton.style.color = 'red'
+    gameTurn()
     console.log(gameSequence,'level ' + level)
     updateLevel()
-}
-
-playerTurn = function() {
-// player clicks buttons, button lights up (and plays sound?)
-    //clickGrnBtn()
-    //clickRedBtn()
-    //clickBluBtn()
-    //clickYelBtn()
-// button color pushed into playerSequence
-    
-// playerSequence is compared to gameSequence
-    for (i = 0; i < playerSequence.length; i++) {
-        if (gameSequence[i] !== playerSequence[i]) {
-            goToNextLevel = false
-            console.log('try again')
-        } else {
-        goToNextLevel = true
-        console.log('move to the next level')
-        }
-    }
-// if no exact match loser message is displayed
-    if (goToNextLevel = true)
-        level += 1 
-// if match exact match, nextLevel is called
-    updateLevel()
-    nextLevel()
-    console.log(playerSequence, level)
-}
-
-nextLevel = function() {
-// get random color for gameplay
-     let randomClr = colors[Math.floor(Math.random() * colors.length)]
-// random color lights up buttons and pushes into gameSequence 
-    if (gameSequence[i] = 'green') {
-        lightGrnBtn = function() {
-            grnButton.style.backgroundColor = '#03cb03';
-            setTimeout(() => {
-                grnButton.style.backgroundColor = 'rgb(0, 61, 0)'
-              }, 500);
-              gameSequence.push('green')
-        }
-    } else if (randomClr = 'red') {
-        lightRedBtn = function() {
-            redButton.style.backgroundColor = '#ff0000'
-            setTimeout(() => {
-                redButton.style.backgroundColor = 'rgb(64, 1, 1)'
-              }, 500);
-              gameSequence.push('red')
-        } 
-    } else if (randomClr = 'blue') {
-        lightBluBtn = function() {
-            bluButton.style.backgroundColor = '#0000ff'
-            setTimeout(() => {
-                bluButton.style.backgroundColor = 'rgb(2, 2, 83)'
-            }, 500);
-                gameSequence.push('blue')
-        }
-    } else if (randomClr = 'yellow') {
-        lightYelBtn = function() {
-            yelButton.style.backgroundColor = '#ffff00'
-            setTimeout(() => {
-                yelButton.style.backgroundColor = 'rgb(43, 43, 1)'
-              }, 500);
-              gameSequence.push('yellow')
-        }
-    }
-// push color into game array
-    gameSequence.push(randomClr)
-// when level goes up, number of randomBtn's returned goes up to match level#
-   for (i = 0; i < gameSequence.length; i++) {
-        if (gameSequence.length < level) {
-            nextLevel()
-        } else {
-            console.log('your turn')
-        }
-    }                                                           
-// // level # is displayed in the level field on screen
-//     let levelText = document.getElementById('#level-counter')
-//     levelText.innerText('level counter:' + level)
-//     // console to check any of this crap is working...    
-//     console.log(gameSequence, level)
 }
 
 clickResetBtn = function() {
@@ -155,6 +75,102 @@ clickResetBtn = function() {
     startButton.style.color = 'rgb(0, 133, 133)'
     console.log(gameSequence, playerSequence, 'level ' + level, 'reset button clicked')
 }
+
+//game play functions
+
+gameTurn = function() {
+// get random color for gameplay
+     let randomClr = colors[Math.floor(Math.random() * colors.length)]
+     //pushes into gameSequence 
+     gameSequence.push(randomClr)
+     updateLevel()
+     console.log(gameSequence, level)
+     // random color lights up buttons and 
+for (i = 0; i < gameSequence.length; i++) {
+     if (gameSequence.length < level) {
+         //... something goes here
+     } 
+    if (gameSequence[i] = 'green') {
+        lightGrnBtn() 
+        gameSequence.push('green')
+    } else if (randomClr = 'red') {
+        lightRedBtn()
+        gameSequence.push('red') 
+    } else if (randomClr = 'blue') {
+        lightBluBtn()
+        gameSequence.push('blue')
+    } else if (randomClr = 'yellow') {
+        lightYelBtn()
+        gameSequence.push('yellow')
+        }
+// push color into game array
+    
+// when level goes up, number of randomBtn's returned goes up to match level#
+                                                       
+// // level # is displayed in the level field on screen
+//     let levelText = document.getElementById('#level-counter')
+//     levelText.innerText('level counter:' + level)
+//     // console to check any of this crap is working...    
+//     console.log(gameSequence, level)
+    }
+}
+
+playerTurn = function() {
+    // player clicks buttons, button lights up (and plays sound?)
+        //clickGrnBtn()
+        //clickRedBtn()
+        //clickBluBtn()
+        //clickYelBtn()
+    // button color pushed into playerSequence
+        
+    // playerSequence is compared to gameSequence
+        for (i = 0; i < playerSequence.length; i++) {
+            if (gameSequence[i] !== playerSequence[i]) {
+                goToNextLevel = false
+                console.log('try again')
+            } else {
+            goToNextLevel = true
+            console.log('move to the next level')
+            }
+        }
+    // if no exact match loser message is displayed
+        if (goToNextLevel = true)
+            level += 1 
+    // if match exact match, nextLevel is called
+        updateLevel()
+        gameTurn()
+        console.log(playerSequence, level)
+    }
+
+//button and light functions
+
+lightGrnBtn = function() {
+    grnButton.style.backgroundColor = '#03cb03'
+    setTimeout(() => {
+        grnButton.style.backgroundColor = 'rgb(0, 61, 0)'
+      }, 500);
+    }
+
+lightRedBtn = function() {
+    redButton.style.backgroundColor = '#ff0000'
+    setTimeout(() => {
+        redButton.style.backgroundColor = 'rgb(64, 1, 1)'
+        }, 500)
+    }
+
+lightBluBtn = function() {
+    bluButton.style.backgroundColor = '#0000ff'
+    setTimeout(() => {
+        bluButton.style.backgroundColor = 'rgb(2, 2, 83)'
+    }, 500)
+}
+
+lightYelBtn = function() {
+    yelButton.style.backgroundColor = '#ffff00'
+    setTimeout(() => {
+        yelButton.style.backgroundColor = 'rgb(43, 43, 1)'
+      }, 500);
+    }
 
 //**event listeners**//
 
