9f0fd0c40d601f1637805f1d529eec74a3afccbf
owentashlin <owentashlin@gmail.com>
2022-10-18 20:24:24 -0700

    changed player turn function, bug fixing

--- js/main.js
+++ js/main.js
@@ -10,24 +10,24 @@ const yelButton = document.querySelector('.yellow-btn')
 const startButton = document.querySelector('#start-button')
 const resetButton = document.querySelector('#reset-button')
 const colors = ['green', 'red', 'blue', 'yellow']
+
+let gameText = document.querySelector('instructions')
 let gameSequence = []
 let playerSequence = []
 let level = 0  
 
 //**functions**//
 
-//color button functions - until we figure out how to do it better, with a toggle
-
 function updateLevel() {
     document.getElementById('level-display').innerHTML = level
-  }
+};
 
 clickStartBtn = function() {
     startButton.style.color = 'red'
     gameTurn()
-    console.log('start button clicked')
+    console.log('game start')
     updateLevel()
-}
+};
 
 clickResetBtn = function() {
     gameSequence = []
@@ -35,75 +35,94 @@ clickResetBtn = function() {
     level = 0
     updateLevel()
     startButton.style.color = 'rgb(0, 133, 133)'
+    document.getElementById('level-display').innerHTML = ''
     console.log(gameSequence, playerSequence, 'level ' + level, 'reset button clicked')
-}
+};
 
-//**game play functions**\\
+//*// game play functions
 
 gameTurn = function() {
-//game iterates through array, repeats finding colors until number of items in array equals level#    
+//*// game iterates through array, repeats finding colors until number of items in array equals level#    
     level += 1 
-    for (i = 0; i < level; i++) {
-        SelectRandomColor() 
-    }
-    console.log(gameSequence)
-    setTimeout(() => {
-        playerTurn()
-      }, 2000);
-}
+    // for (i = 0; i < level; i++) {
+    //     if (i < level) {
+    //     SelectRandomColor() 
+    //     }
+    // }
+    SelectRandomColor()
+    gameSequence.forEach(showGameSequence)
+    console.log(gameSequence, 'level ' + level, 'your turn')
+        setTimeout(() => {
+            playerTurn()
+        }, levelTimer())
+};
 
-//player clicks buttons, loading answers into player array, when done, calls compareArrays function
+//*// player clicks buttons, loading answers into player array, when done, calls compareArrays function
 playerTurn = function () {
     for (i = 0; i < playerSequence.length; i++)
         if(playerSequence.length === level) {
             console.log('did it')
             compareArrays()
-        } else {
-            //something to say to keep going
+        } else if (playerSequence < level) {
+            // something here to do stuff
         }
     console.log(playerSequence)    
-}
+};
 
 compareArrays = function() {
-// playerSequence is compared to gameSequence
+//*// playerSequence is compared to gameSequence
     for (i = 0; i < playerSequence.length; i++) {
         if (gameSequence[i] !== playerSequence[i]) {
-// if no match, you lose message displayed   
-        clickResetBtn()
+//*// if no match, you lose message displayed   
         console.log('try again')
-        } else {
-// if match exact match, nextLevel is called
+        } else if (gameSequence[i] === playerSequence[i]) {
+//*// if match exact match, nextLevel is called
     updateLevel()
         console.log('move to the next level')
+        }
     }
-}
     playerSequence = []    
     gameTurn()
         console.log(playerSequence, level)
-    }
+};
 
-//button and light functions
+//*// button and light functions
 
-// get random color for gameplay
+//*// get random color for gameSequence
 let SelectRandomColor = function() {
 let randomClr = colors[Math.floor(Math.random() * colors.length)]
-// random color lights up buttons and 
     if (randomClr === 'green') {
-        lightGrnBtn()
         gameSequence.push('green')
     } else if (randomClr === 'red') {
-        lightRedBtn()
         gameSequence.push('red')
     } else if (randomClr === 'blue') {
-        lightBluBtn()
         gameSequence.push('blue')
     } else if (randomClr === 'yellow') {
-        lightYelBtn()
         gameSequence.push('yellow')
         }
 }
 
-//player click functions
+showGameSequence = function() {
+    if (randomClr === 'green') {
+        lightGrnBtn()
+    } else if (randomClr === 'red') {
+        lightRedBtn()
+    } else if (randomClr === 'blue') {
+        lightBluBtn()
+    } else if (randomClr === 'yellow') {
+        lightYelBtn()
+        }
+}
+
+levelTimer = function() {
+    //need to put in a timer function to graduate up the time per level based on the level number
+}
+
+// setTimeout(() => {
+//     playerTurn()
+// }, 1500)
+
+//*// player click functions
 
 clickGrnBtn = function() {
     grnButton.style.backgroundColor = '#03cb03';
@@ -111,7 +130,6 @@ clickGrnBtn = function() {
         grnButton.style.backgroundColor = 'rgb(0, 61, 0)'
       }, 500);
       playerSequence.push('green')
-    //console.log('player moves', playerSequence)
 }
 
 clickRedBtn = function() {
@@ -120,7 +138,6 @@ clickRedBtn = function() {
         redButton.style.backgroundColor = 'rgb(64, 1, 1)'
       }, 500);
       playerSequence.push('red')
-    //console.log('player moves', playerSequence)
 }
 
 clickBluBtn = function() {
@@ -129,7 +146,6 @@ clickBluBtn = function() {
         bluButton.style.backgroundColor = 'rgb(2, 2, 83)'
     }, 500);
         playerSequence.push('blue')
-    //console.log('player moves', playerSequence)
 }
 
 clickYelBtn = function() {
@@ -138,10 +154,9 @@ clickYelBtn = function() {
         yelButton.style.backgroundColor = 'rgb(43, 43, 1)'
       }, 500);
         playerSequence.push('yellow')
-    //console.log('player moves', playerSequence)
 }
 
-//light-up button functions
+//*// light-up button functions
 lightGrnBtn = function() {
     grnButton.style.backgroundColor = '#03cb03'
     setTimeout(() => {
@@ -170,7 +185,7 @@ lightYelBtn = function() {
       }, 500);
     }
 
-//**event listeners**//
+//*// event listeners
 
 grnButton.addEventListener('click', clickGrnBtn)
 redButton.addEventListener('click', clickRedBtn)
