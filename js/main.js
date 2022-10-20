12af4eb171a40bfd706580ee491fbbcec6ec2b33
owentashlin <owentashlin@gmail.com>
2022-10-19 21:48:44 -0700

    fixed bugs, added player win

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 console.log("bugs! bugs, everywhere! AAAAAAA!!!!!")
 
 //*// elements
-
 const grnButton = document.querySelector('.green-btn')
 const redButton = document.querySelector('.red-btn')
 const bluButton = document.querySelector('.blue-btn')
@@ -9,11 +8,13 @@ const yelButton = document.querySelector('.yellow-btn')
 const startButton = document.querySelector('#start-button')
 const resetButton = document.querySelector('#reset-button')
 const colors = ['green', 'red', 'blue', 'yellow']
-
 let gameText 
 let gameSequence = []
+//let gameSequenceCounter = 0
 let playerSequence = []
-let level = 0  
+//let playerSequenceCounter = 0
+let level = 1 
+
 
 //*// functions
 function updateLevel() {
@@ -25,9 +26,9 @@ function updateGameText() {
 }
 
 clickStartBtn = function() {
+    //playerWins()
     startButton.style.color = 'red'
     gameTurn()
-    console.log('game start')
     updateLevel()
 }
 
@@ -44,103 +45,163 @@ clickResetBtn = function() {
 }
 
 //*// game play functions
-
-gameTurn = function() {
-//*// game iterates through array, repeats finding colors until number of items in array equals level#    
+gameTurn = function() { 
+    //gameSequence.length = level  
     gameText = 'Wait and watch'
-    updateGameText()
-    level += 1 
-    updateLevel()
-    SelectRandomColor()
-    gameSequence.forEach(showGameSequence)
-    console.log(gameSequence, 'level ' + level)
+        updateGameText()
+    level +=1
+        updateLevel()
+        pushRandomColor(randomClr())
+    console.log(gameSequence)
+    for (let i = 1; i <= gameSequence.length; i++)    
+        setTimeout(showGameSequence(gameSequence[i - 1]), (1000 * i))
+    // console.log(gameSequence)
         setTimeout(() => {
             playerTurn()
         }, gameLevelTimer())
 }
 
-//*// player clicks buttons, loading answers into player array, when done, calls compareArrays function
 playerTurn = function () {
+    playerSequence = []
     gameText = 'Your Turn'
     updateGameText()
-    for (i = 0; i < playerSequence.length; i++)
-        if(playerSequence.length === level) {
-            console.log('did it')
-            setTimeout(() => {
-                compareArrays()
-            }, playerTurnTimer())
-        } else if (playerSequence < level) {
-            // something here to do stuff
-        }
+        setTimeout(() => {
+            compareArrays()
+        }, playerTurnTimer())
     console.log(playerSequence)    
 }
 
 compareArrays = function() {
-//*// playerSequence is compared to gameSequence
+let arrayMatch    
     for (i = 0; i < playerSequence.length; i++) {
         if (gameSequence[i] !== playerSequence[i]) {
-//*// if no match, you lose message displayed   
-        console.log('try again')
+            arrayMatch != true 
+            {break}
         } else if (gameSequence[i] === playerSequence[i]) {
-//*// if match exact match, nextLevel is called
-    updateLevel()
-        console.log('move to the next level')
+            arrayMatch = true 
+            {continue}
         }
-    }
-    playerSequence = []    
-    gameTurn()
+    }    
+        if (arrayMatch !== true) {
+            clickResetBtn()
+        } else if (arrayMatch === true) {
+            gameTurn()
+            console.log('match')
+        }
+    // if (level === 5)
+    //     playerWins()    
         console.log(playerSequence, level)
-}
+    }    
 
 //*// button and light functions
 
-//*// get random color for gameSequence
-let randomClr = colors[Math.floor(Math.random() * colors.length)]
+let randomClr = function() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
 
-let SelectRandomColor = function() {
-    if (randomClr === 'green') {
+let pushRandomColor = function(color) {
+    if (color === 'green') {
         gameSequence.push('green')
-    } else if (randomClr === 'red') {
+    } else if (color === 'red') {
         gameSequence.push('red')
-    } else if (randomClr === 'blue') {
+    } else if (color === 'blue') {
         gameSequence.push('blue')
-    } else if (randomClr === 'yellow') {
+    } else if (color === 'yellow') {
         gameSequence.push('yellow')
         }
 }
 
-showGameSequence = function() {
-    if (randomClr === 'green') {
-        lightGrnBtn()
-    } else if (randomClr === 'red') {
-        lightRedBtn()
-    } else if (randomClr === 'blue') {
-        lightBluBtn()
-    } else if (randomClr === 'yellow') {
-        lightYelBtn()
+//remove timers
+showGameSequence = function(color) {
+    if (color === 'green') {
+        setTimeout(lightGrnBtn(), 2000)
+    } else if (color === 'red') {
+        setTimeout(lightRedBtn(), 2000)
+    } else if (color === 'blue') {
+        setTimeout(lightBluBtn(), 2000)
+    } else if (color === 'yellow') {
+        setTimeout(lightYelBtn(), 2000)
         }
 }
 
 gameLevelTimer = function() {
-    if (level <= 3){
-        return 2500
-    } else if (level <= 5){
-        return 4000
-    } else if (level <= 10){
-        return 10000
+    if (level <= 5){
+        return 1500
+    // } else if (level <= 5){
+    //     return 2000
+    // } else if (level <= 10){
+    //     return 6000
     }
 }
 
 playerTurnTimer = function() {
-    if (level <= 3){
-        return 4000
-    } else if (level <= 5){
-        return 6000
-    } else if (level <= 10){
-        return 15000
+    if (level === 5){
+        return 3000
+    // } else if (level <= 5){
+    //     return 6000
+    // } else if (level <= 10){
+    //     return 9000
+    // } else if (level <= 20){
+    //     return 15000
     }
 }
 
+playerWins = function() {
+    if (level === 1) {
+    //animation plays
+        setTimeout(() => {
+            lightGrnBtn()
+        }, 100)
+        setTimeout(() => {
+            lightRedBtn()
+        }, 200)
+        setTimeout(() => {
+            lightBluBtn()
+        }, 300)
+        setTimeout(() => {
+            lightYelBtn()
+        }, 400)
+        setTimeout(() => {
+            lightGrnBtn()
+        }, 500)
+        setTimeout(() => {
+            lightRedBtn()
+        }, 600)
+        setTimeout(() => {
+            lightBluBtn()
+        }, 700)
+        setTimeout(() => {
+            lightYelBtn()
+        }, 800)
+        setTimeout(() => {
+            lightGrnBtn()
+        }, 900)
+        setTimeout(() => {
+            lightRedBtn()
+        }, 1000)
+        setTimeout(() => {
+            lightBluBtn()
+        }, 1100)
+        setTimeout(() => {
+            lightYelBtn()
+        }, 1200)
+        setTimeout(() => {
+            lightGrnBtn()
+        }, 1300)
+        setTimeout(() => {
+            lightRedBtn()
+        }, 1400)
+        setTimeout(() => {
+            lightBluBtn()
+        }, 1500)
+        setTimeout(() => {
+            lightYelBtn()
+        }, 1600)
+       //message is displayed in box
+    gameText = "you win!"
+    updateGameText()
+    }
+}
 
 //*// player click functions
 
@@ -150,6 +211,7 @@ clickGrnBtn = function() {
         grnButton.style.backgroundColor = 'rgb(0, 61, 0)'
       }, 500);
       playerSequence.push('green')
+      console.log(playerSequence, 'clicked green')
 }
 
 clickRedBtn = function() {
@@ -158,6 +220,7 @@ clickRedBtn = function() {
         redButton.style.backgroundColor = 'rgb(64, 1, 1)'
       }, 500);
       playerSequence.push('red')
+      console.log(playerSequence, 'clicked red')
 }
 
 clickBluBtn = function() {
@@ -165,7 +228,8 @@ clickBluBtn = function() {
     setTimeout(() => {
         bluButton.style.backgroundColor = 'rgb(2, 2, 83)'
     }, 500);
-        playerSequence.push('blue')
+    playerSequence.push('blue')
+        console.log(playerSequence, 'clicked blue')
 }
 
 clickYelBtn = function() {
@@ -173,7 +237,8 @@ clickYelBtn = function() {
     setTimeout(() => {
         yelButton.style.backgroundColor = 'rgb(43, 43, 1)'
       }, 500);
-        playerSequence.push('yellow')
+      playerSequence.push('yellow')
+        console.log(playerSequence, 'clicked yellow')
 }
 
 //*// light-up button functions
